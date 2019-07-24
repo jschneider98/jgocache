72c81775a5f78182d0ca45046a243e906105242e
James Schneider <jschneider98@gmail.com>
2019-07-24 15:03:28 -0700

    Simplified factory interface. Added a missing precache conditional check.

--- autocert/cache/cache.go
+++ autocert/cache/cache.go
@@ -30,19 +30,16 @@ import (
 
 var _ autocert.Cache = &Cache{}
 
-// CacheBackend type serves to specify cache backend type
-type CacheBackend string
-
 const (
 	// CacheBackendDir specifies autocert's native DirCache caching backend
 	// Which uses local directory for storing certificates
-	CacheBackendDir CacheBackend = "dir"
+	CacheBackendDir string = "dir"
 
 	// CacheBackendRedis specifies redis caching driver
-	CacheBackendRedis CacheBackend = "redis"
+	CacheBackendRedis string = "redis"
 
 	// CacheBackendSQL specifies database/sql family of caching drivers(MySQL, PostgreSQL, etc.)
-	CacheBackendSQL CacheBackend = "sql"
+	CacheBackendSQL string = "sql"
 )
 
 // Cache struct to store high level cache instance
@@ -60,14 +57,17 @@ type Cache struct {
 // If encryption is turned on Get will try to decrypt data retrieved from
 // backend's Get before filling cache and returning the data.
 func (c *Cache) Get(ctx context.Context, key string) ([]byte, error) {
+
 	if c.usePrecaching {
 		data, ok := c.precache.Load(key)
+
 		if ok {
 			return data.([]byte), nil
 		}
 	}
 
 	data, err := c.backend.Get(ctx, key)
+
 	if err != nil {
 		return nil, err
 	}
@@ -78,11 +78,14 @@ func (c *Cache) Get(ctx context.Context, key string) ([]byte, error) {
 	}
 
 	data, err = c.decrypt(data)
+
 	if err != nil {
 		return nil, err
 	}
 
-	c.precache.Store(key, data)
+	if c.usePrecaching {
+		c.precache.Store(key, data)
+	}
 
 	return data, nil
 }
@@ -94,6 +97,7 @@ func (c *Cache) Get(ctx context.Context, key string) ([]byte, error) {
 // but precache will be filled with plaintext data.
 func (c *Cache) Put(ctx context.Context, key string, data []byte) error {
 	var resultData []byte
+
 	if c.encryptionKey != nil {
 		var err error
 		resultData, err = c.encrypt(data)
@@ -120,14 +124,17 @@ func (c *Cache) Put(ctx context.Context, key string, data []byte) error {
 // If precache is used it will first delete from cache no matter if backend's
 // Put completed successfully or failed.
 func (c *Cache) Delete(ctx context.Context, key string) error {
+
 	if c.usePrecaching {
 		c.precache.Delete(key)
 	}
+
 	return c.backend.Delete(ctx, key)
 }
 
 func (c *Cache) decrypt(ciphertext []byte) ([]byte, error) {
 	block, err := aes.NewCipher(c.encryptionKey)
+
 	if err != nil {
 		return nil, err
 	}
@@ -135,6 +142,7 @@ func (c *Cache) decrypt(ciphertext []byte) ([]byte, error) {
 	if len(ciphertext) < aes.BlockSize {
 		return nil, fmt.Errorf("Ciphertext is too short. Probably corrupted data")
 	}
+
 	iv := ciphertext[:aes.BlockSize]
 	ciphertext = ciphertext[aes.BlockSize:]
 
@@ -147,12 +155,14 @@ func (c *Cache) decrypt(ciphertext []byte) ([]byte, error) {
 
 func (c *Cache) encrypt(plaintext []byte) ([]byte, error) {
 	block, err := aes.NewCipher(c.encryptionKey)
+
 	if err != nil {
 		return nil, err
 	}
 
 	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
 	iv := ciphertext[:aes.BlockSize]
+
 	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
 		return nil, err
 	}
@@ -164,17 +174,25 @@ func (c *Cache) encrypt(plaintext []byte) ([]byte, error) {
 }
 
 // NewCacheFactory returns Cache instance
-func NewCacheFactory(backend CacheBackend, options map[string]string) (autocert.Cache, error) {
+func NewCacheFactory(options map[string]string) (autocert.Cache, error) {
 	var err error
+
+	backend, ok := options["backend"]
+
+	if !ok {
+		return nil, fmt.Errorf("Missing required 'backend' parameter")
+	}
 
 	var usePrecaching bool
 	usePrecachingString, ok := options["usePrecaching"]
+
 	if ok {
 		usePrecaching, _ = strconv.ParseBool(usePrecachingString)
 	}
 
 	var encKey []byte
 	encryptionKey, ok := options["encryptionKey"]
+
 	if ok && encryptionKey != "" {
 		h := sha256.New()
 		h.Write([]byte(encryptionKey))
@@ -183,22 +201,26 @@ func NewCacheFactory(backend CacheBackend, options map[string]string) (autocert.
 	}
 
 	var b autocert.Cache
+
 	switch backend {
-	case CacheBackendSQL:
-		b, err = newSQLCacheBackend(options)
-		if err != nil {
-			return nil, err
-		}
-	case CacheBackendDir:
-		b, err = newDirCacheBackend(options)
-		if err != nil {
-			return nil, err
-		}
-	case CacheBackendRedis:
-		b, err = newRedisCacheBackend(options)
-		if err != nil {
-			return nil, err
-		}
+		case CacheBackendSQL:
+			b, err = newSQLCacheBackend(options)
+
+			if err != nil {
+				return nil, err
+			}
+		case CacheBackendDir:
+			b, err = newDirCacheBackend(options)
+
+			if err != nil {
+				return nil, err
+			}
+		case CacheBackendRedis:
+			b, err = newRedisCacheBackend(options)
+
+			if err != nil {
+				return nil, err
+			}
 	}
 
 	var c autocert.Cache = &Cache{
@@ -213,6 +235,7 @@ func NewCacheFactory(backend CacheBackend, options map[string]string) (autocert.
 
 func newDirCacheBackend(options map[string]string) (autocert.Cache, error) {
 	dir, ok := options["path"]
+
 	if !ok {
 		return nil, fmt.Errorf("Option 'path' is required for 'dir' cache backend")
 	}
@@ -230,25 +253,33 @@ func newDirCacheBackend(options map[string]string) (autocert.Cache, error) {
 
 func newSQLCacheBackend(options map[string]string) (autocert.Cache, error) {
 	driver, ok := options["driver"]
+
 	if !ok {
 		return nil, fmt.Errorf("No driver specified")
 	}
+
 	dsn, ok := options["dsn"]
+
 	if !ok {
 		return nil, fmt.Errorf("dsn option to backend is required")
 	}
+
 	db, err := sql.Open(driver, dsn)
+
 	if err != nil {
 		return nil, fmt.Errorf("Error establishing database connection: %s", err)
 	}
+
 	if e := db.Ping(); e != nil {
 		return nil, fmt.Errorf("Error contacting database: %s", e)
 	}
+
 	return sqlcache.NewCache(db)
 }
 
 func newRedisCacheBackend(options map[string]string) (autocert.Cache, error) {
 	addr, ok := options["addr"]
+
 	if !ok || addr == "" {
 		return nil, fmt.Errorf("No 'addr' specified for Redis cache")
 	}
@@ -256,11 +287,13 @@ func newRedisCacheBackend(options map[string]string) (autocert.Cache, error) {
 	password, _ := options["password"]
 
 	dbS, ok := options["db"]
+
 	if !ok {
 		dbS = "0"
 	}
 
 	db, err := strconv.Atoi(dbS)
+
 	if err != nil {
 		return nil, fmt.Errorf("Error parsing db field: %s", err)
 	}
